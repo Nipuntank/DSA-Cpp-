5cc80afd217815e8d9181b64d154f3e0935c05f5
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2022-10-12 03:37:38 +0530

    Create printlevelorder.cpp

---
+++ printlevelorder.cpp
@@ -0,0 +1,39 @@
+#include <queue>
+#include <vector>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<T> *> children;
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
+TreeNode<int> *takeinput()
+{
+    int rootData;
+    cout << "Enter the data:";
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+    queue<TreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        TreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cout << "Enter the num of child "<<front->data<<":";
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++)
+        {
+            int childData;
+            cout << "Enter " << i << "th child of " << front->data << ":";
+            cin >> childData;
+            TreeNode<int> *child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
